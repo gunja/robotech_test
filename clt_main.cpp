68cdb72828c49324a3e39af20032d3221ef6b990
Ivan F Gushchin @ DellCrow 18 <konara@ya.ru>
2023-12-03 15:23:04 +0400

    initial structure of applications

---
+++ clt_main.cpp
@@ -0,0 +1,12 @@
+/**
+\file clt_main.cpp - start point of client
+application.
+*/
+#include <iostream>
+
+int main(int argc, const char *argv[])
+{
+    std::cout <<"Hello world, I'm client interactive side"<<std::endl;
+    return 0;
+}
+
---
+++ srv_main.cpp
@@ -0,0 +1,12 @@
+/**
+\file srv_main.cpp - start point of server
+application.
+*/
+#include <iostream>
+
+int main(int argc, const char *argv[])
+{
+    std::cout <<"Hello world, I'm server side"<<std::endl;
+    return 0;
+}
+
